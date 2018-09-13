df67e3ee837d3d21edf14c4a9de4fe11ee23007a
Rana <42984871+Rana2019@users.noreply.github.com>
2018-09-13 21:25:25 +0300

    Add Addresses Page

---
+++ TestGithub/TestGithub/Addresses.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestGithub
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Addresses : ContentPage
+	{
+		public Addresses ()
+		{
+			InitializeComponent ();
+
+            // Hello , this is Addresses Page ^_^ 
+		}
+	}
+}
